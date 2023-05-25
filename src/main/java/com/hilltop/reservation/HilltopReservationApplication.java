894d4f6cb71a6f2c8eff4df882a82c8e2bbfbd8d
SangeethArulraj <sangeeth@swivelgroup.com.au>
2023-05-25 14:29:31 +0530

    eureka changes made

--- src/main/java/com/hilltop/reservation/HilltopReservationApplication.java
+++ src/main/java/com/hilltop/reservation/HilltopReservationApplication.java
@@ -2,8 +2,10 @@ package com.hilltop.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HilltopReservationApplication {
 
 	public static void main(String[] args) {
