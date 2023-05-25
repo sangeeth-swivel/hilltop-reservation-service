be7e920adf4ccf997fe00fa23570ddad32a1d988
SangeethArulraj <sangeeth@swivelgroup.com.au>
2023-05-25 10:17:06 +0530

    Initial commit

---
+++ src/main/java/com/hilltop/reservation/HilltopReservationApplication.java
@@ -0,0 +1,13 @@
+package com.hilltop.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HilltopReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HilltopReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hilltop/reservation/HilltopReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilltop.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HilltopReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
